0b26efcc810649197b614ffc4921d93270975c59
Benjamin Chiang <chiangbenjamin991@gmail.com>
2023-04-07 01:23:10 -0700

    feat(eslint): add typescript-eslint config type to exported config

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ const tsRules = {
   '@typescript-eslint/no-non-null-assertion': 0,
 };
 
+/** @type {import('@typescript-eslint/utils').TSESLint.Linter.Config} */
 module.exports = {
   env: {
     node: true,
